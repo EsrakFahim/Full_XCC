6c6e7b68f9d2d439abc6935675bd7721d3dae9b0
EsrakFahim <efahim0656@gmail.com>
2024-12-22 01:43:23 +0600

    Reduce rate limit window to 1 minute and update related message

--- backend/src/app.js
+++ backend/src/app.js
@@ -59,10 +59,10 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Rate Limiting
 const limiter = rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
+      windowMs: 1 * 60 * 1000, // 15 minutes
       max: 100, // Limit each IP
       message: {
-            message: "Too many requests from this IP, please try again after 15 minutes",
+            message: "Too many requests from this IP, please try again after 1 minutes",
       },
 });
 app.use(limiter);
